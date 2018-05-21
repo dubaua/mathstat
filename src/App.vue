006c2e8999c8da287cfefa7af602967a4019461b
dubaua <dubaua@gmail.com>
2018-05-21 10:53:47 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  #app
+    | Я родился
+    product
+</template>
+
+<script>
+import Product from '@/components/product';
+
+export default {
+  name: 'App',
+  components: {
+    Product,
+  },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss"></style>
